3d2fce58694badf79eda95c8854972baae32b4c5
Louis Brunet <louis.brunet1@grenoble-inp.org>
2024-09-08 14:57:26 +0200

    chore: replace template default values for project setup

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+OBS Tournament Plugin
+Copyright (C) 2024 Louis Brunet <louis.jc.brunet@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
